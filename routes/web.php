f7c137aca5eed4fe1fbdd8ea59ad6c30584ee5ad
Norackvinhou <74971465+Norackvinhou@users.noreply.github.com>
2021-06-02 22:21:04 +0700

    Intergrated Theme

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about.html', function () {
+    return view('FE.about');
+});
+Route::get('/index.html', function () {
+    return view('welcome');
+});
+Route::get('/team.html', function () {
+    return view('FE.team');
+});
+Route::get('/services.html', function () {
+    return view('FE.service');
+});
+Route::get('/testimonials.html', function () {
+    return view('FE.test');
+});
+Route::get('portfolio.html', function () {
+    return view('FE.portfolio');
+});
+Route::get('/pricing.html', function () {
+    return view('FE.pricing');
+});
+Route::get('/blog.html', function () {
+    return view('FE.blog');
+});
+Route::get('/contact.html', function () {
+    return view('FE.contact');
+});
+
+
